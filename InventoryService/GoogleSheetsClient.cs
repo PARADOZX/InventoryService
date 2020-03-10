75005a96e70ce85a64fb43d08e7822735962d549
James <jcc9283@hotmail.com>
2020-03-09 20:33:37 -0400

    initial commit

---
+++ InventoryService/GoogleSheetsClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryService
+{
+    public class GoogleSheetsClient
+    {
+        private readonly string _clientID = "353833583458-8ve6nb4vurj856tostcpiji30o3lvvdf.apps.googleusercontent.com";
+        private readonly string _clientSecret = "t6NY4l8NOkWAPwIi6L8sUJz7";
+        public GoogleSheetsClient()
+        {
+
+        }
+    }
+}
